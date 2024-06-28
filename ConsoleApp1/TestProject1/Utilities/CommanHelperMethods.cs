9b07dacdad04fe5e40fa1e9fefc92bc304d615e8
parthiban murugan <c.m.parthiban1999@gmail.com>
2024-06-29 00:37:07 +0530

    adding a screenshot for specific element

--- ConsoleApp1/TestProject1/Utilities/CommanHelperMethods.cs
+++ ConsoleApp1/TestProject1/Utilities/CommanHelperMethods.cs
@@ -1,6 +1,7 @@
 ﻿using OpenQA.Selenium;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,6 +25,21 @@ namespace TestProject1.Utilities
 
             var dir = $"{filePath}\\{screenshotName}";
             ss.SaveAsFile($"{dir}.png");
+        }
+
+        public Bitmap MakeElemScreenshot(IWebDriver driver, IWebElement elem, string filePath, string screenshotName)
+        {
+            Screenshot myScreenShot = ((ITakesScreenshot)driver).GetScreenshot();            
+            using (var screenBmp = new Bitmap(new MemoryStream(myScreenShot.AsByteArray)))
+            {
+                 screenBmp.Clone(new Rectangle(elem.Location, elem.Size), screenBmp.PixelFormat);
+                if (!Directory.Exists(filePath))
+                {
+                    Directory.CreateDirectory(filePath);
+                }
+                screenBmp.Save($"{filePath}\\{screenshotName}.png");
+                return screenBmp;
+            }
         }
     }
 }
